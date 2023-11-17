c372d114c6e883316fa86665c634f831d13906e6
ozillo <marcmateo25@gmail.com>
2023-11-17 14:21:54 +0100

    inicio componentes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css"
+
+
+import React from 'react'
+
+const Header = () => {
+  return (
+   <header>
+    <div className="socialMedia">linkedin</div>
+    <div>CodePen</div>
+    <div>GitHub</div>
+    
+    </header>
+  )
+}
+
+export default Header

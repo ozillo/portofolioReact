e9ea1846a1e55ae439503053c63ba4538ac87d9f
ozillo <marcmateo25@gmail.com>
2023-11-18 11:35:23 +0100

    Header,Footer & Home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,15 @@ import React from 'react'
 const Header = () => {
   return (
    <header>
-    <div className="socialMedia">linkedin</div>
-    <div>CodePen</div>
-    <div>GitHub</div>
+    <div className="logo"><img src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1699904682/PortofolioMarcMateo/LOGOTEMPLATE_ccohy5.png" alt="" /></div>
+    <div className="socialMedia1">
+        <ul className="socialMedia">
+            <li>Linkedin</li>
+            <li>GitHub</li>
+            <li>Codepen</li>
+        </ul>
+    </div>
+    
     
     </header>
   )

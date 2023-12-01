4bfa37f9f4dc559aac1abb8e17ad7e467a4c86e2
ozillo <marcmateo25@gmail.com>
2023-12-01 23:58:44 +0100

    Add h1 Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
   return (
    <header>
     <div className="logo">
+      <h1>Portofolio</h1>
       <img 
       src="https://res.cloudinary.com/dw9b8eqmc/image/upload/v1699904682/PortofolioMarcMateo/LOGOTEMPLATE_ccohy5.png" alt="logo" className="logo"/>
       

1bd6968b21322aa52e22d73c8586ab08ca4e9dfa
ozillo <marcmateo25@gmail.com>
2024-01-11 12:11:30 +0100

    Add functions on NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,19 +3,32 @@ import "./NavBar.css"
 import React from 'react'
 
 const NavBar = () => {
+  const changeTheme = () => {
+    document.body.classList.toggle("light");
+    changeText(); 
+  };
+  
+     const changeText = () => {
+    let themeBtn = document.querySelector("#themeBtn");
+    if (themeBtn.innerText === "☀") {
+      themeBtn.innerText = "☾";
+    } else {
+      themeBtn.innerText = "☀";
+    }
+  };
   return (
     <nav>
   <h2>Marc Mateo</h2>
   <ul>
       <li>
-          <a href="#" id="homelink">Home</a>
+      <a href="#" id="homelink">Home</a>
       </li>
       <li>
-          <a href="#" id="projectslink">Proyectos</a>
+      <a href="#" id="projectslink">Proyectos</a>
       </li>
-      <li>
-          <button id="themeBtn">☀</button>
-      </li>
+        <li>
+          <button onClick={()=>changeTheme} id="themeBtn">☀</button>
+       </li>
   </ul>
   </nav>
   )

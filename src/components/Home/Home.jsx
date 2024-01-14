6ddeb6774158e7ae258928e0dcb1fe0e4ecc9f71
ozillo <marcmateo25@gmail.com>
2024-01-14 16:07:51 +0100

    Add neon style on Home title

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,9 +11,12 @@ const Home = () => {
     <section className="home">
     <div class="loader"></div>
       {/* <p>Hola, Soy</p> */}
-
+      <div class="container">
+    <div class="neon">Fullstack </div>
+    <div class="flux">Developer </div>
+    </div>
       
-      <h2>Fullstack Developer</h2> 
+      {/* <h2>Fullstack Developer</h2>  */}
       <p>Me dedico al FullStack a partir de Javascript, donde implemento mis habilidades en las distintas<br></br>
       tecnologías tanto en la parte Front-end como en la parte Back-end para poder manejar e dirigir los datos deseados<br></br> y su diseño acorde con las características de la aplicación buscada.</p>
       <a href="mailto:marmateodvlpr@gmail.com">e-mail →</a>

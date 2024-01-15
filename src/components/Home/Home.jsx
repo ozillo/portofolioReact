8c8cc252c590a91b0cf9e1a1916b8ed786120d96
ozillo <marcmateo25@gmail.com>
2024-01-15 18:09:11 +0100

    Changes on home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,8 +9,6 @@ const Home = () => {
     <>
     
     <section className="home">
-
-      
       <h2>Fullstack Developer</h2>  
       <p>Me dedico al FullStack a partir de Javascript, donde implemento mis habilidades en las distintas<br></br>
       tecnologías tanto en la parte Front-end como en la parte Back-end para poder manejar e dirigir los datos deseados<br></br> y su diseño acorde con las características de la aplicación buscada.</p>

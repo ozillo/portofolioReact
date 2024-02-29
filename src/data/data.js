1a55023800161df82fcea16aed1e38d85a92fcf1
ozillo <marcmateo25@gmail.com>
2024-02-29 18:07:47 +0100

    changes on page

--- src/data/data.js
+++ src/data/data.js
@@ -10,7 +10,7 @@ export const data = [
     },
     {
       title: "Saüc Floristeria",
-      description: "Esta es la descripción de mi segundo proyecto",
+      description: "Floristeria local",
       image:
         "https://res.cloudinary.com/dw9b8eqmc/image/upload/v1701634348/PortofolioMarcMateo/macbookSauc_sngifv.png",
       tech: ["React", "HTML5", "CSS3", "JavaScript"],
@@ -18,32 +18,32 @@ export const data = [
       link: "www.proyecto1.com"
     },
     {
-      title: "Tercer proyecto",
-      description: "Esta es la descripción de mi tercer proyecto",
+      title: "Fem camí",
+      description: "Centro especializado en autismo",
       image:
         "https://images.unsplash.com/photo-1669570094762-828f3dfaf675?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
       tech: ["React", "HTML5", "CSS3", "JavaScript"],
       github: "http://github.com/myuser/proyecto3",
       link: "www.proyecto1.com",
-    },
-    {
-      title: "Cuarto proyecto",
-      description: "Esta es la descripción de mi cuarto proyecto",
-      image:
-        "https://images.unsplash.com/photo-1664575198308-3959904fa430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
-      tech: ["React", "HTML5", "CSS3", "JavaScript"],
-      github: "http://github.com/myuser/proyecto4",
-      link: "www.proyecto1.com",
-    },
-    {
-      title: "Quinto proyecto",
-      description: "Esta es la descripción de mi quinto proyecto",
-      image:
-        "https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80",
-      tech: ["React", "HTML5", "CSS3", "JavaScript"],
-      github: "http://github.com/myuser/proyecto5",
-      link: "www.proyecto1.com",
-    },
+    }
+    // {
+    //   title: "Cuarto proyecto",
+    //   description: "Esta es la descripción de mi cuarto proyecto",
+    //   image:
+    //     "https://images.unsplash.com/photo-1664575198308-3959904fa430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+    //   tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    //   github: "http://github.com/myuser/proyecto4",
+    //   link: "www.proyecto1.com",
+    // },
+    // {
+    //   title: "Quinto proyecto",
+    //   description: "Esta es la descripción de mi quinto proyecto",
+    //   image:
+    //     "https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80",
+    //   tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    //   github: "http://github.com/myuser/proyecto5",
+    //   link: "www.proyecto1.com",
+    // },
   ];
 
   

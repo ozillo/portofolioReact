3d1226dfc15aec4f68cfc05bef7ebd663d66e1d9
ozillo <marcmateo25@gmail.com>
2024-12-09 19:39:37 +0100

    update new page named skills

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,86 @@ import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
 import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
 
+
+// function App() {
+//   const [showHome, setShowHome] = useState(true);
+
+//   return (
+//    <>
+//    <Header/>
+//     <main>
+//       <div className='All-btn'>
+//           <button
+//               className="custom-btn"
+//               onClick={() => setShowHome(true)}
+//             >
+//               Home
+//             </button>
+            
+//           <button
+//               className="custom-btn"
+//               onClick={() => setShowHome(false)}
+//             >
+//               Proyectos
+//           </button>
+//       </div>
+//             <div>
+//         {showHome ? (
+//           <Home home={Home} />
+//         ) : (
+//           <Projects projects={Projects} />
+//         )}
+//         </div>
+//     </main>
+//     <Footer/>
+   
+//    </>
+//   )
+
+   
+// }
+
+// export default App
 
 function App() {
-  const [showHome, setShowHome] = useState(true);
+  const [activeComponent, setActiveComponent] = useState('home'); // Puede ser 'home', 'projects', o 'habilidades'.
 
   return (
-   <>
-   <Header/>
-    <main>
-      <div className='All-btn'>
+    <>
+      <Header />
+      <main>
+        <div className="All-btn">
           <button
-              className="custom-btn"
-              onClick={() => setShowHome(true)}
-            >
-              Home
-            </button>
-            
-          <button
-              className="custom-btn"
-              onClick={() => setShowHome(false)}
-            >
-              Proyectos
+          className={`custom-btn ${activeComponent === 'home' ? 'active' : ''}`}
+          onClick={() => setActiveComponent('home')}
+          >
+            Home
           </button>
-      </div>
-            <div>
-        {showHome ? (
-          <Home home={Home} />
-        ) : (
-          <Projects projects={Projects} />
-        )}
+          <button
+            className="custom-btn"
+            onClick={() => setActiveComponent('projects')}
+          >
+            Proyectos
+          </button>
+          <button
+            className="custom-btn"
+            onClick={() => setActiveComponent('skills')}
+          >
+            Habilidades
+          </button>
         </div>
-    </main>
-    <Footer/>
-   
-   </>
-  )
-
-   
+        <div>
+          {activeComponent === 'home' && <Home />}
+          {activeComponent === 'projects' && <Projects />}
+          {activeComponent === 'skills' && <Skills />}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
+
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,33 +1,93 @@
-import { data } from "../../data/data"
-import "./Projects.css"
-import React from 'react'
-
+import { data } from "../../data/data";
+import "./Projects.css";
+import React from 'react';
 
 const Projects = () => {
   return (
-   
-  
-   <section className="projects-section">
-    <h1 className="projectsH1">Proyectos</h1>
-    <span className="divider"></span>
-    
-      {data.map((project)=>{
-        return (
-          <figure className="figureProjects">
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-          <img className="imgFigure" src={project.image} alt="imgCard"/>
-          <p>{project.tech}</p>
-          <p>{project.github}</p>
-          <p>{project.link}</p>
-        </figure>
-        )
-      })
-      
-      }
-   </section>
-   
-  )
-}
+    <div>
+      <section className="projects-section">
+        <h1 className="projectsH1">Proyectos</h1>
+        <span className="divider"></span>
 
-export default Projects
+        {data.map((project) => {
+          return (
+            <figure className="figureProjects" key={project.title}>
+              <h2>{project.title}</h2>
+              <p>{project.description}</p>
+              <img className="imgFigure" src={project.image} alt="imgCard" />
+              <p>{project.tech}</p>
+              <p>
+                <a href={project.github} target="_blank" rel="noopener noreferrer">
+                  GitHub
+                </a>
+              </p>
+              <p>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                  Live Demo
+                </a>
+              </p>
+            </figure>
+          );
+        })}
+      </section>
+
+      <section className="cards-wrapper">
+        <div className="card-grid-space">
+          <a
+            className="card"
+            href="https://frontend-final-project-ip0mr7t7q.vercel.app/"
+            style={{ backgroundImage: "url(https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg)" }}
+          >
+            <div>
+              <h1>DevLink</h1>
+              <p>The syntax of a language is how it works. How to actually write it. Learn HTML syntax…</p>
+              <div className="tags">
+                <div className="tag">HTML</div>
+                <div className="tag">CSS</div>
+                <div className="tag">REACT</div>
+                <div className="tag">NODEJS</div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="card-grid-space">
+          <a
+            className="card"
+            href="http://xn--sacfloristeria-hsb.cat/"
+            style={{ backgroundImage: "url('https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/basic-types-of-html-tags/cover.jpg')" }}
+          >
+            <div>
+              <h1>Saüc Floristeria</h1>
+              <p>Learn about some of the most common HTML tags…</p>
+              <div className="tags">
+                <div className="tag">WORDPRESS</div>
+                <div className="tag">PHP</div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="card-grid-space">
+          <a
+            className="card"
+            href="https://femcami.cat/"
+            style={{ backgroundImage: "url('https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/links-images-about-file-paths/cover.jpg')" }}
+          >
+            <div>
+              <h1>Fem cami</h1>
+              <p>Learn how to use links and images along with file paths…</p>
+              <div className="tags">
+                <div className="tag">HTML</div>
+                <div className="tag">CSS</div>
+                <div className="tag">REACT</div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Projects;
